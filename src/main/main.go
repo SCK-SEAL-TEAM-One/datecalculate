304ea4ee92999dc3203bc780f198d54b5f81459f
CAPTAINAMERICA <captainamerica@CAPTAINAMERICAs-Mac-mini.local>
2018-06-13 09:57:49 +0700

    [Edit]  แก้ไขไฟล์ main.go

--- src/main/main.go
+++ src/main/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	fs := http.FileServer(http.Dir("static"))
-	http.Handle("/web", fs)
+	http.Handle("/web/", http.StripPrefix("/web/", fs))
 
 	log.Println("Listening...")
 	http.ListenAndServe(":3000", nil)
