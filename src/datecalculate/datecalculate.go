b165e1abf9d07634cd1275ad8adc16fa8430ef64
natshenghou <dai_00333@hotmail.com>
2018-06-13 15:30:57 +0700

    [Add]:ฟังก์ชั่นแปลงวันเป็นชั่วโมงกับฟังก์ชันแปลงวันเป็นสัปดาห์กับแปลงวันเป็นนาทีและฟังก์ชันเปอร์เซ็นของปี

--- src/datecalculate/datecalculate.go
+++ src/datecalculate/datecalculate.go
@@ -15,3 +15,28 @@ func FormatDate(day, month, year int) string {
 
 	return fmt.Sprintf("%s, %s %s %s", convertWeekDay.String(), strconv.Itoa(convertDay), convertMonth.String(), strconv.Itoa(convertYear))
 }
+
+func DayToHour(days int) string {
+	hour := days * 24
+
+	return fmt.Sprintf("%s hours", strconv.Itoa(hour))
+}
+
+func PercentOfYear(days int) string {
+	numberPercentOfYear := (float64(days) / 365) * 100
+
+	return fmt.Sprintf("%s of common year(365 days)", strconv.FormatFloat(numberPercentOfYear, 'f', 2, 64))
+}
+
+func DayToMinute(days int) string {
+	minute := days * 24 * 60
+
+	return fmt.Sprintf("%s minutes", strconv.Itoa(minute))
+}
+
+func DayToWeek(days int) string {
+	week := days / 7
+	overday := days % 7
+
+	return fmt.Sprintf("%s weeks and %s days", strconv.Itoa(week), strconv.Itoa(overday))
+}
