5a1a0cfd5ef9c029d601ffda4414fca4e216e81f
nsyxkiller <i_miyamoto_ok@hotmail.com>
2018-06-14 11:48:16 +0700

    [Edited]: เพิ่มเทสของ MakeJson และเขียน code ทำให้ผ่าน test

--- src/datecalculate/dateCalculate_test.go
+++ src/datecalculate/dateCalculate_test.go
@@ -32,3 +32,25 @@ func Test_NewtDate_Input_4_6_2018_Should_Be_4_6_2018_Time_date(t *testing.T) {
 	}
 
 }
+
+func Test_MakeJson_Input_startDate_4_1_2018_endDate_4_6_2018_Should_Be_Return_DurationResponse(t *testing.T) {
+	startDate := time.Date(2018, 1, 4, 0, 0, 0, 0, time.UTC)
+	endDate := time.Date(2018, 6, 4, 0, 0, 0, 0, time.UTC)
+	expected := DurationResponse{
+		From:         "Thursday, 4 January 2018",
+		To:           "Monday, 4 June 2018",
+		Days:         "152 days",
+		Years:        "5 months, 1 day",
+		Seconds:      "13,132,800 seconds",
+		Minutes:      "218,880 minutes",
+		Hours:        "3,648 hours",
+		Weeks:        "21 weeks and 5 days",
+		RatioOfYears: "41.64% of 2018",
+	}
+	actualResponse := MakeJson(startDate, endDate)
+
+	if expected != actualResponse {
+		t.Errorf("Should be %v but got %v", expected, actualResponse)
+	}
+
+}
