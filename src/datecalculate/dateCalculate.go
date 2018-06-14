ad622a82116c9d8284d26ed164ff515ec104e7fe
ployploy <chonnikan.t@kkumail.com>
2018-06-14 12:05:12 +0700

    [Edited]: แก้ไขฟังก์ชัน DurationBetweenDate

--- src/datecalculate/dateCalculate.go
+++ src/datecalculate/dateCalculate.go
@@ -9,9 +9,7 @@ func NewDate(day, month, year int) time.Time {
 	return time.Date(year, time.Month(month), day, 0, 0, 0, 0, time.UTC)
 }
 
-func DurationBetweenDate(startDay, startMonth, startYear, endDay, endMonth, endYear int) int {
-	startDate := NewDate(startDay, startMonth, startYear)
-	endDate := NewDate(endDay, endMonth, endYear)
+func DurationBetweenDate(startDate, endDate time.Time) int {
 	diff := endDate.Sub(startDate)
 	days := diff.Hours()/HOUR + 1
 
