8abd7c6e6e8f31345fb143e06d150cf0f6bc89a2
ployploy <chonnikan.t@kkumail.com>
2018-06-14 12:17:38 +0700

    [Edited]: เพิ่มฟังก์ชัน FormatDay

--- src/datecalculate/dateCalculate.go
+++ src/datecalculate/dateCalculate.go
@@ -1,17 +1,26 @@
 package datecalculate
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 const HOUR = 24
 
 func NewDate(day, month, year int) time.Time {
-
 	return time.Date(year, time.Month(month), day, 0, 0, 0, 0, time.UTC)
 }
 
 func DurationBetweenDate(startDate, endDate time.Time) int {
 	diff := endDate.Sub(startDate)
 	days := diff.Hours()/HOUR + 1
-
 	return int(days)
+}
+
+func FormatDay(days int) string {
+	if days < 2 {
+		return fmt.Sprintf("%d day", days)
+	}
+	return fmt.Sprintf("%d days", days)
+
 }
