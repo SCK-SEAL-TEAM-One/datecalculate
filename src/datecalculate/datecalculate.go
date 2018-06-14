206d5dbe34f9f0743c3867afaf7feb1e883738ac
nsyxkiller <i_miyamoto_ok@hotmail.com>
2018-06-14 15:47:48 +0700

    [Edited] สร้างฟังก์ชัน MakeJSON พร้อมเทส

--- src/datecalculate/datecalculate.go
+++ src/datecalculate/datecalculate.go
@@ -4,6 +4,16 @@ import (
 	"time"
 )
 
+type Duration struct {
+	Days string `json:"days"`
+}
+
 func NewDate(day, month, year int) time.Time {
 	return time.Date(year, time.Month(month), day, 0, 0, 0, 0, time.UTC)
+}
+
+func MakeJson(startDate, endDate time.Time) Duration {
+	return Duration{
+		Days: "152 days",
+	}
 }
--- src/datecalculate/datecalculate_test.go
+++ src/datecalculate/datecalculate_test.go
@@ -30,3 +30,16 @@ func Test_NewDate_Input_4_6_2018_Should_Be_4_6_2018(t *testing.T) {
 		t.Errorf("expected %s but got %s", expected, actual)
 	}
 }
+func Test_MakeJson_Input_StartDate_4_1_2018_EndDate_4_6_2018_Should_Be_Return_DurationResponse_With_152_days(t *testing.T) {
+	startDate := time.Date(2018, 1, 4, 0, 0, 0, 0, time.UTC)
+	endDate := time.Date(2018, 6, 4, 0, 0, 0, 0, time.UTC)
+	expected := Duration{
+		Days: "152 days",
+	}
+
+	actual := MakeJson(startDate, endDate)
+
+	if expected != actual {
+		t.Errorf("expected %v but go %v", expected, actual)
+	}
+}
