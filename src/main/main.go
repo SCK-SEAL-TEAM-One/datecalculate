9a3745d1db496bec6744fd6eabe29631c0b61d35
nsyxkiller <i_miyamoto_ok@hotmail.com>
2018-06-14 11:34:41 +0700

    [Edited]: แก้ไขให้ path /web/ สามารถแสดงผลไฟล์ขึ้น[C

--- src/main/main.go
+++ src/main/main.go
@@ -8,8 +8,8 @@ import (
 
 func main() {
 	fs := http.FileServer(http.Dir("static"))
-	http.Handle("/web", http.StripPrefix("/web", fs))
 	http.HandleFunc("/duration", api.ApiCalculateDate)
+	http.Handle("/web/", http.StripPrefix("/web", fs))
 
 	log.Println("Listening...")
 	http.ListenAndServe(":3000", nil)
